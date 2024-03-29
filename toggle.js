a2d03d8e15a6990799a86363f4bc382ee42b1f07
Juditmoon <145652363+Juditmoon@users.noreply.github.com>
2024-03-29 17:35:57 +0100

    Create toggle.js

---
+++ toggle.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('btn')
+
+function leftClick() {
+    btn.style.left = "0%"
+}
+
+function rightClick() {
+    btn.style.left = "25px"
+}
+
+console.log(rightClick)
+
+
